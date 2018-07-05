66dcf26a52523aa8355c1456fe467fb06e105339
yoshihikosuzuki <ys.neoteny@gmail.com>
2018-07-05 11:50:57 +0200

    fixed setup.py

--- dacenter/setup.py
+++ dacenter/setup.py
@@ -1,20 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-with open('README.md') as f:
-    readme = f.read()
-
-with open('requirements.txt') as f:
-    requirements = f.read().strip().split('\n')
-
-
-setup(
-    name="dacenter",
-    version="0.1.0",
-    description=("DAzzler CENTromere clustERing module"),
-    long_description=readme,
-    author="Yoshihiko Suzuki",
-    author_email="ys.neoteny@gmail.com",
-    install_requires=requirements,
-    packages=find_packages(exclude=('tests', 'docs'))
-)
+setup()
