f6d58d4957dfb8ccd66c306f991726598c86d81b
yoshihikosuzuki <ys.neoteny@gmail.com>
2019-06-28 15:06:38 +0900

    revert

--- src/main.py
+++ src/main.py
@@ -7,27 +7,34 @@ from BITS.util.log import print_log
 from .datander import run_datander
 from .datruf import run_datruf
 
-tasks = ['all', 'datander', 'datruf', 'dacmaster', 'dalayout']
+tasks = ["all", "datander", "datruf", "dacmaster", "dalayout"]
 
 
 @dataclass(repr=False, eq=False)
 class VCA:
-    '''Config file must be TOML-formatted.'''
-    task_name    : str   # TODO: in the end only 'all' would be needed, so delete this
+    """Config file must be TOML-formatted.
+    A simple example from REPL is:
+      > from vca import VCA
+      > v = VCA("all", "/path/to/config")
+      > v.run()
+    """
+    task_name    : str   # TODO: in the end only "all" would be needed, so delete this
     config_fname : InitVar[str]
     config       : dict         = field(init=False)
     scheduler    : Scheduler    = field(init=False, default=None)
 
     def __post_init__(self, config_fname):
-        assert isfile(config_fname), f'No config file: {config_fname}'
-        self.config = toml.load(config_fname)
-        assert 'db_prefix' in self.config, 'Config must have a value "db_prefix"'
+        assert self.task_name in tasks, f"Invalid task name: {self.task_name}"
 
-        if self.config.get('job_scheduler', {}).get('enabled', False):
-            self.scheduler = Scheduler(**self.config['job_scheduler'].get('params', {}))
+        assert isfile(config_fname), f"No config file: {config_fname}"
+        self.config = toml.load(config_fname)
+        assert "db_prefix" in self.config, "Config must have a value 'db_prefix'"
+
+        if self.config.get("job_scheduler", {}).get("enabled", False):
+            self.scheduler = Scheduler(**self.config["job_scheduler"].get("params", {}))
 
     def run(self):
-        eval(f'self._run_{self.task_name}()')
+        eval(f"self._run_{self.task_name}()")
 
     def _run_all(self):
         self._run_datander()
@@ -35,38 +42,38 @@ class VCA:
         #self._run_dacmaster()
         #self._run_dalayout()
 
-    @print_log('datander')
+    @print_log("datander")
     def _run_datander(self):
-        run_datander(self.config['db_prefix'],
-                     **self.config.get('datander', {}),
+        run_datander(self.config["db_prefix"],
+                     **self.config.get("datander", {}),
                      scheduler=self.scheduler)
 
-    @print_log('datruf')
+    @print_log("datruf")
     def _run_datruf(self):
-        run_datruf(f'{self.config["db_prefix"]}.db',
-                   f'TAN.{self.config["db_prefix"]}.las',
-                   **self.config.get('datruf', {}),
+        run_datruf(f"{self.config['db_prefix']}.db",
+                   f"TAN.{self.config['db_prefix']}.las",
+                   **self.config.get("datruf", {}),
                    scheduler=self.scheduler)
 
-    @print_log('dacmaster')
+    @print_log("dacmaster")
     def _run_dacmaster(self):
         pass
 
-    @print_log('dalayout')
+    @print_log("dalayout")
     def _run_dalayout(self):
         pass
 
 
 def load_args():
-    p = argparse.ArgumentParser(description='VCA: Vertebrate Centromere Assembler.')
-    p.add_argument('task_name', type=str, default='all',
-                   help=f'Task name. {{{", ".join(tasks)}}} [all]')
-    p.add_argument('config_fname', type=str, default='config',
-                   help='TOML-formatted config file. [config]')
+    p = argparse.ArgumentParser(description="VCA: Vertebrate Centromere Assembler.")
+    p.add_argument("task_name", type=str, default="all",
+                   help=f"Task name. {{{', '.join(tasks)}}} [all]")
+    p.add_argument("config_fname", type=str, default="config",
+                   help="TOML-formatted config file. [config]")
     return p.parse_args()
 
 
 def main():
     args = load_args()
-    vca = VCA(args.task_name, args.config_fname)
-    vca.run()
+    v = VCA(args.task_name, args.config_fname)
+    v.run()
--- src/read_viewer.py
+++ src/read_viewer.py
@@ -13,7 +13,7 @@ class ReadViewer:
     gepard: str
 
     def __post_init__(self):
-        run_command(f'mkdir -p {self.out_dir}')
+        run_command(f"mkdir -p {self.out_dir}")
 
     def show(self, read, dot_plot=False, alignment_plot=True, size=1000, out_html=None):
         if dot_plot:
@@ -28,46 +28,46 @@ class ReadViewer:
         tr_alignments = filter_alignments(tr_intervals, alignments)
 
         read_len = len(read.seq)
-        shapes = [make_line(0, 0, read_len, read_len, 'grey', 3)]   # diagonal (= read)
+        shapes = [make_line(0, 0, read_len, read_len, "grey", 3)]   # diagonal (= read)
         for start, end in tr_intervals:   # on diagonal
-            shapes.append(make_line(start, start, end, end, 'black', 3))
+            shapes.append(make_line(start, start, end, end, "black", 3))
 
         for ab, ae, bb, be, distance, slope in alignments:
             if (ab, ae, bb, be) in tr_alignments:
-                col, width = 'purple', 3
+                col, width = "purple", 3
             else:
-                col = 'black' if 0.95 <= slope <= 1.05 else 'yellow'
+                col = "black" if 0.95 <= slope <= 1.05 else "yellow"
                 width = 1
             shapes.append(make_line(ab, bb, ae, be, col, width))   # self alignment
 
         traces = [make_scatter([x[0] for x in alignments],   # ab
                                [x[2] for x in alignments],   # bb
-                               name='start'),
+                               name="start"),
                   make_scatter([x[1] for x in alignments],   # ae
                                [x[3] for x in alignments],   # be
-                               name='end'),
+                               name="end"),
                   make_scatter([x[0] for x in tr_intervals],
                                [x[0] for x in tr_intervals],
                                text=list(range(1, len(tr_intervals) + 1)),
-                               text_pos='top right', text_size=10, text_col='grey', mode='text',
-                               name='TR interval')]
+                               text_pos="top right", text_size=10, text_col="grey", mode="text",
+                               name="TR interval")]
 
-        if hasattr(read, 'units'):
-            shapes += [make_line(unit.start, unit.start, unit.end, unit.end, 'black', 5)
+        if hasattr(read, "units"):
+            shapes += [make_line(unit.start, unit.start, unit.end, unit.end, "black", 5)
                        for unit in read.units]
             traces += [make_scatter([unit.start for unit in read.units],
                                     [unit.start for unit in read.units],
-                                    text=[f'{i} ' for i in range(len(list(read.units)))],
-                                    text_pos='bottom left', text_size=10, text_col='black', mode='text',
-                                    name='TR unit'),
+                                    text=[f"{i} " for i in range(len(list(read.units)))],
+                                    text_pos="bottom left", text_size=10, text_col="black", mode="text",
+                                    name="TR unit"),
                        make_scatter([unit.start for unit in read.units],
                                     [unit.start for unit in read.units],
-                                    text=[f'unit {i}<br>{unit.start}:{unit.end}'
+                                    text=[f"unit {i}<br>{unit.start}:{unit.end}"
                                           for i, unit in enumerate(read.units)],
-                                    col='black',
+                                    col="black",
                                     show_legend=False)]
 
-        layout = make_layout(size, size, title=f'{read.id}',
+        layout = make_layout(size, size, title=f"{read.id}",
                              x_range=[-read_len * 0.05, read_len + 100],
                              y_range=[0, read_len],
                              x_grid=False, y_grid=False, y_reversed=True, shapes=shapes)
@@ -75,6 +75,6 @@ class ReadViewer:
         show_plot(traces, layout, out_html)
 
     def _dot_plot(self, read):
-        out_fasta = f'{self.out_dir}/{read.id}.fasta'
-        run_command(f'DBshow {self.db_file} {read.id} > {out_fasta}')
+        out_fasta = f"{self.out_dir}/{read.id}.fasta"
+        run_command(f"DBshow {self.db_file} {read.id} > {out_fasta}")
         DotPlot(self.gepard, self.out_dir).plot_fasta(out_fasta, out_fasta)
