8cb3b0c9a68c8fe86c391b8ebc819a85d545515b
yoshihikosuzuki <ys.neoteny@gmail.com>
2019-11-16 00:28:54 +0900

    small fix

--- src/labeled_reads_viewer.py
+++ src/labeled_reads_viewer.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from matplotlib.colors import XKCD_COLORS
 from BITS.plot.plotly import make_line, make_scatter, make_layout, show_plot
+from .overlapper.ava_labeled_reads import _ava_labeled_reads
 
 
 def show_labeled_read(read, max_read_len):
@@ -39,18 +40,19 @@ def show_labeled_reads(labeled_reads):
         show_labeled_read(read, max_length)
 
 
-def show_labeled_reads_pileup(read_id, labeled_reads_by_id, overlaps, height=300):
+def show_labeled_reads_pileup(read_id, labeled_reads_by_id, height=None):
     labeled_reads = labeled_reads_by_id[read_id]
     labeled_reads_by_id_single = {read.id: read for read in labeled_reads}
 
     rel_start_pos = defaultdict(list)
     rel_start_pos[read_id].append(0)
 
-    for o in overlaps:
+    for o in _ava_labeled_reads(labeled_reads):
         if o.a_read_id == read_id:
             rel_start_pos[o.b_read_id].append(o.a_start - o.b_start)
         elif o.b_read_id == read_id:
-            rel_start_pos[o.a_read_id].append(o.b_start - o.a_start)
+            rel_start_pos[o.a_read_id].append(o.b_start - o.a_start if o.strand == 0
+                                              else (o.b_len - o.b_end) - (o.a_len - o.a_end))
 
     x_min = min([min(poss) for read_id, poss in rel_start_pos.items()])
     x_max = max([max(poss) + labeled_reads_by_id_single[read_id].length
@@ -68,7 +70,8 @@ def show_labeled_reads_pileup(read_id, labeled_reads_by_id, overlaps, height=300
         id_to_col = {}
         for i, col in enumerate(XKCD_COLORS.values()):   # NOTE: up to ~1000 cols
             id_to_col[i] = col
-        shapes += [make_line(start_pos + unit.start, y, start_pos + unit.end, y, id_to_col[unit.repr_id], 5, 'above')
+        shapes += [make_line(start_pos + unit.start, y, start_pos + unit.end, y,
+                             id_to_col[unit.repr_id], 5, 'above')
                    for i, unit in enumerate(read.units)]
         trace_unit = make_scatter([start_pos + unit.start for i, unit in enumerate(read.units)],
                                   [y for unit in read.units],
@@ -83,7 +86,12 @@ def show_labeled_reads_pileup(read_id, labeled_reads_by_id, overlaps, height=300
         traces += [trace_unit, trace_info]
         y -= 1
 
-    layout = make_layout(height=height, x_range=(x_min, x_max),
-                         x_grid=False, y_grid=False, x_zeroline=False,
+    traces += [make_scatter([start_pos for start_pos, read_id in start_pos_to_read],
+                            [-y for y in range(len(start_pos_to_read))],
+                            text=[read_id for start_pos, read_id in start_pos_to_read],
+                            col="grey", marker_size=1, show_legend=False)]
+
+    layout = make_layout(height=100 + 50 * len(start_pos_to_read) if height is None else height,
+                         x_range=(x_min, x_max), x_grid=False, y_grid=False, x_zeroline=False,
                          shapes=shapes)
     show_plot(traces, layout)
